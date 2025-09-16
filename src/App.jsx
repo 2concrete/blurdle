7f74cad921abf695355c289ffcebe96df85cc49d
2concrete <danielpeace654@gmail.com>
2025-09-16 09:06:34 +0800

    got unsplash api request working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { createApi } from "unsplash-js";
+
 const App = () => {
-  return <div>App</div>;
+  const [image, setImage] = useState("");
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const unsplash = createApi({ accessKey: apiKey });
+
+  const fetchImage = () => {
+    unsplash.photos.getRandom({ count: 1 }).then((result) => {
+      if (result.errors) {
+        console.error("Error fetching:", result.errors);
+      } else {
+        console.log("Photo data:", result.response);
+      }
+    });
+  };
+
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <button onClick={fetchImage}>click</button>
+    </div>
+  );
 };
 
 export default App;
